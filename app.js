ce6511d39c2c12463d1490e5fe4f925996054503
Rabiu Kabir <rabiu.kabir@aun.edu.ng>
2022-11-24 16:57:00 +0100

    New update

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const morgan = require('morgan');
+
+// const AppError = require('./utils/appError');
+// const globalErrorHandler = require('./controllers/errorController');
+ const postRouter = require('./routes/postRoutes');
+// const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 3) ROUTES
+app.use('/api/v1/post', postRouter);
+// app.use('/api/v1/users', userRouter);
+
+// app.all('*', (req, res, next) => {
+//   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+// });
+
+// app.use(globalErrorHandler);
+
+module.exports = app;
