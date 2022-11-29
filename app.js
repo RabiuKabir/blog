941de171aec2e6a5585edd741cec6d06ffd9f254
Rabiu Kabir <rabiu.kabir@aun.edu.ng>
2022-11-29 17:48:57 +0100

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 // const AppError = require('./utils/appError');
 // const globalErrorHandler = require('./controllers/errorController');
  const postRouter = require('./routes/postRoutes');
-// const userRouter = require('./routes/userRoutes');
+ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 app.use('/api/v1/post', postRouter);
-// app.use('/api/v1/users', userRouter);
+app.use('/api/v1/users', userRouter);
 
 // app.all('*', (req, res, next) => {
 //   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
