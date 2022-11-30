8d5d1f00b066e241209753b92009b71c70d0c76c
Rabiu Kabir <rabiu.kabir@aun.edu.ng>
2022-11-30 21:04:26 +0100

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
 //The routes
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
 router
   .route('/')
   .get(userController.getAllUsers)
