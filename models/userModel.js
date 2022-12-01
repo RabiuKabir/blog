dc2ac057a1e774a98c62c03215f3fe325c3b8efa
Rabiu Kabir <rabiu.kabir@aun.edu.ng>
2022-12-01 17:12:51 +0100

    update

--- models/userModel.js
+++ models/userModel.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Please tell us your name!']
+    required: [true, 'Please provide your name!']
   },
   email: {
     type: String,
