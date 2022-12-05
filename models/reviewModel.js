c50fe5a4f79bde2a889bd238adb98cbf7d0714ee
Rabiu Kabir <rabiu.kabir@aun.edu.ng>
2022-12-05 21:24:50 +0100

    Add review model

---
+++ models/reviewModel.js
@@ -0,0 +1,103 @@
+// review / rating / createdAt / ref to post / ref to user
+const mongoose = require('mongoose');
+const Post = require('./postModel');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty!']
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Post',
+      required: [true, 'Review must belong to a post.']
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user']
+    }
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+reviewSchema.index({ post: 1, user: 1 }, { unique: true });
+
+reviewSchema.pre(/^find/, function(next) {
+  // this.populate({
+  //   path: 'post',
+  //   select: 'name'
+  // }).populate({
+  //   path: 'user',
+  //   select: 'name photo'
+  // });
+
+  this.populate({
+    path: 'user',
+    select: 'name photo'
+  });
+  next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function(postId) {
+  const stats = await this.aggregate([
+    {
+      $match: { post: postId }
+    },
+    {
+      $group: {
+        _id: '$post',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+  // console.log(stats);
+
+  if (stats.length > 0) {
+    await Post.findByIdAndUpdate(postId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating
+    });
+  } else {
+    await Post.findByIdAndUpdate(postId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5
+    });
+  }
+};
+
+reviewSchema.post('save', function() {
+  // this points to current review
+  this.constructor.calcAverageRatings(this.post);
+});
+
+// findByIdAndUpdate
+// findByIdAndDelete
+reviewSchema.pre(/^findOneAnd/, async function(next) {
+  this.r = await this.findOne();
+  // console.log(this.r);
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function() {
+  // await this.findOne(); does NOT work here, query has already executed
+  await this.r.constructor.calcAverageRatings(this.r.post);
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
