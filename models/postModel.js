fdaf7f8f4014bb7f18b3fcd181aac5f1faaa0fee
Rabiu Kabir <rabiu.kabir@aun.edu.ng>
2022-12-16 09:57:39 +0100

    update

--- models/postModel.js
+++ models/postModel.js
@@ -47,7 +47,10 @@ postSchema.pre(/^find/, function(next) {
   //   path: 'user',
   //   select: 'name photo'
   // });
-  
+   //  .populate({
+  //   path: 'user',
+  //   select: 'name photo'
+  // });
   
 
   next();
